e62954c190db431e1b8a2ce41b032ad5ebd23a09
Orvin Bellamy <orvinbellamy101@gmail.com>
2024-07-12 21:59:47 -0700

    update work

--- filehandler.py
+++ filehandler.py
@@ -1,8 +1,38 @@
 import pandas as pd
 import openai
 from openai import OpenAI
+import json
 
 class FileHandler:
-    def __init__(self, df: pd.DataFrame):
+    def __init__(self, df: pd.DataFrame, dic_file: dict, file_name: str, file_path: str, client: OpenAI):
         
+        self._client = client
+        self._dic_file = dic_file
         self.df = df
+        self.file_name = file_name
+        self.file_path = file_path
+        self.file_id = dic_file[file_name]
+
+        # Make sure the csv is always updated
+        df.to_csv(f"{file_path}{file_name}", index=False)
+
+    # Delete the file from openai and 
+    def update_openai_file(self):
+        
+        # Try deleting file from openai
+        try:
+            self._client.files.delete(self.file_id)
+        # If file already doesn't exist
+        except:
+            print(f"file name: {self.file_name}, file id: {self.file_id} doesn't exist or is already deleted")
+
+        openai_file = self._client.files.create(
+            file=open(f"{self.file_path}{self.file_name}", 'rb'),
+            purpose='assistants'
+        )
+
+        self._dic_file[self.file_name] = openai_file.id
+        self.file_id = openai_file.id
+
+        with open(f"{self.file_path}{self.file_name}", 'w') as json_file:
+            json.dump(self._dic_file, json_file)
--- yfinancehandler.py
+++ yfinancehandler.py
@@ -1,3 +1,50 @@
 import pandas as pd
 import yfinance as yf
 
+class YFHandler():
+    def __init__(self, stock_list: str, schemas: dict):
+        
+        self.stocks = yf.Tickers(stock_list)
+        self.stock_list = stock_list
+        self.schemas = schemas
+    
+    def import_stocks(self):
+
+        df = pd.DataFrame(columns=dict(self.schemas['stocks']))
+
+        for stock in self.stock_list:
+
+            df_plc = self.stocks.tickers[stock].history(period='max').reset_index()
+            df_plc['Ticker'] = stock
+
+            df = pd.concat([df, df_plc], ignore_index=True)
+
+        return df
+
+    def import_cashflow(self):
+
+        df = pd.DataFrame(columns=dict(self.schemas['cashflow']))
+
+        for stock in self.stock_list:
+
+            df_plc = self.stocks.tickers[stock].cashflow.T.reset_index()
+            df_plc.rename(columns={'index': 'Date'},inplace=True)
+            df_plc['Ticker'] = stock
+
+            df = pd.concat([df, df_plc], ignore_index=True)
+        
+        return df
+    
+    def import_income_stmt(self):
+        
+        df = pd.DataFrame(columns=dict(self.schemas['income_stmt']))
+
+        for stock in self.stock_list:
+
+            df_plc = self.stocks.tickers[stock].income_stmt.T.reset_index()
+            df_plc.rename(columns={'index': 'Date'},inplace=True)
+            df_plc['Ticker'] = stock
+
+            df = pd.concat([df, df_plc], ignore_index=True)
+
+        return df
