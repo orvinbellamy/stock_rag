d6dac95c8d6e54569e8f04986cadd62fa25937d7
Orvin Bellamy <orvinbellamy101@gmail.com>
2024-09-26 00:06:55 -0700

    Beginning new work and minor housekeeping, no functional change

---
+++ systemmanager.py
@@ -0,0 +1,9 @@
+### For managing multiple agents together ###
+
+from agenthandler import AgentHandler
+from eventhandler import ThreadManager
+
+class MultiSystemManager():
+
+	def __init__(self):
+		pass
