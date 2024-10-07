aedbe902973b74a10e5b19d8a92d854d12ded3c8
Orvin Bellamy <orvinbellamy101@gmail.com>
2024-10-07 00:19:40 -0700

    Updating systemmanager

--- systemmanager.py
+++ systemmanager.py
@@ -3,21 +3,23 @@
 from agenthandler import AgentHandler
 from eventhandler import ThreadManager
 
+class Staff:
+    def __init__(self, name: str, title: str, salary: float, subordinates: list[str]):
+        self.name = name
+        self.title = title
+        self.salary = salary
+        self.subordinates = subordinates
+
+    def add_subordinate(self, subordinate: str):
+        self.subordinates.append(subordinate)
+
+    def remove_subordinate(self, subordinate: str):
+        self.subordinates.remove(subordinate)
+
 class MultiSystemManager():
 
-	def __init__(self):
-		pass
+	def __init__(self, staff_list: list[Staff]):
+		self.staff_list = staff_list
 
-
-class ProcessEntity():
-	
-	def __init__(self, name, entity_type='Test', role=None):
-
-		self.name = name
-		self.entity_type = entity_type  # "individual" or "team"
-		self.role = role
-		self.sub_entities = []
-
-	def add_sub_entity(self, sub_entity):
-
-		self.sub_entities.append(sub_entity)
+	def fire(self, staff: Staff):
+		self.staff_list.remove(staff)
